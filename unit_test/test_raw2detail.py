a1f00398c2b441e831160c5f4771f7d1d527e8f6
pedestrain <h15814042386@qq.com>
2023-11-03 05:31:30 +0800

    add ut

--- unit_test/test_raw2detail.py
+++ unit_test/test_raw2detail.py
@@ -1,6 +1,31 @@
+import os
 from unittest import TestCase
 
 from domain.rule.detail_filter.raw2detail import r2d_lishui
+
+
+def read_html_file(filename):
+    """
+    must read file from absolute path to make sure both testable in local and cloud
+
+    :param filename:
+    :return:
+    """
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the absolute path to the file
+    file_path = os.path.join(script_dir, 'raw_content', filename)
+
+    # Open the file, read its content, and return the content
+    with open(file_path, "r", encoding='utf-8') as f:
+        html_content = f.read()
+
+    return html_content
+
+
+# Usage:
+html_content = read_html_file('lishui_detail.html')
 
 
 class Test(TestCase):
@@ -8,9 +33,7 @@ class Test(TestCase):
     #     self.fail()
 
     def test_r2d_lishui(self):
-        with open("raw_content/lishui_detail.html", "r", encoding='utf-8') as f:
-            html_content = f.read()
-        result = r2d_lishui(html_content)
+        result = r2d_lishui(read_html_file('lishui_detail.html'))
         assert len(result.get("title")) > 4
         assert len(result.get("content")) > 40
 
